b7b7540e75c4b527d501e0b95fae3aa11c5f12b9
krishna1797 <krishnaagrawal.1797@gmail.com>
2026-06-25 13:38:40 +0530

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,26 @@
+#practice for if/elif/else by project of learn how to cook
+
+print("Helooo welcome to my program \nTody i will teach u how to make your fav recipe ")
+
+lala=input("Enter your favourate dish:").lower()
+
+if(lala=="paneer"):
+    print("buy 1 kg paneer \n first cut the paneer then add some masala and salt in it")
+    print("add 1 cup water and vegetables \nCOOK COOK COOKK... ")
+    print("DONEEEE \nnow EATTTT :)")
+    
+elif(lala=="pasta"):
+    print("buy pastaaa \nTake bowl add water and pasta in it then add maggie masala and chesse ")
+    print("COOK COOK COOKKK...\n doneeee :)")
+    
+elif(lala=="chicken"):
+    print("wash chicken \ncook chicken")
+    print("makeeee chickennnnn")
+    
+elif(lala=="pizza"):
+    print("buy pizza base \nadd cheese on pizza basee ")
+    print("adddd moreeee cheeseeeeeee")
+    print("EATTTT")
+    
+else:
+    print("order from zomatoooo ")
