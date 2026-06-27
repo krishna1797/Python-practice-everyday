c67d3795de3179a0106086622bdf167b9282c1c4
krishna1797 <krishnaagrawal.1797@gmail.com>
2026-06-27 16:58:12 +0530

    Add files via upload

---
+++ practice5.py
@@ -0,0 +1,23 @@
+#this is a number guessing game using loop/break if elif and else it will also print your number or attempt
+
+
+
+print("Welcome to number game🥳🥳\nHere U have to guess a number between 1 to 100")
+print("And u have to keep guessig until U guess the right number")
+no=18
+count=0
+while True:
+    A=int(input("\n Enter Your Number:"))
+    if (A > no):
+        print("Your guess is too high ☹️")
+        count+=1
+        print("Number of times u have tried:",count)
+    elif (A<no):
+        print("Your guess is too low 😞")
+        count+=1
+        print("Number of times u have tried:",count)
+    else:
+        print("Congratulations U got the corret number🎉💐💐🥳")
+        count+=1
+        print("U won the game in just (",count,") tries")
+        break
